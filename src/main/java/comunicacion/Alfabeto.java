f8bbbe4b10e1c62606a15495f3cd1bcba7d186ec
santiago2904 <44620638+santiago2904@users.noreply.github.com>
2021-04-27 17:22:39 -0500

    Add files via upload

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comunicacion;
+
+/**
+ *
+ * @author santi
+ */
+public class Alfabeto extends Pictograma {
+
+    private String[] letras;
+    private String interpretacion;
+
+    public Alfabeto(String origen, String[] letras, String interpretacion) {
+        super(origen);
+        this.interpretacion = interpretacion;
+        this.letras = letras;
+    }
+
+    public String[] getLetras() {
+        return letras;
+    }
+
+    public void setLetras(String[] letras) {
+        this.letras = letras;
+    }
+
+    public String getInterpretacion() {
+        return interpretacion;
+    }
+
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
+
+    @Override
+    String interpretacion() {
+        return interpretacion; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toString() {
+        String valor="";
+        for(int i =0; i<letras.length;i++){
+            if (i!=letras.length-1){
+                valor+=letras[i]+", ";
+            }else{
+                valor+=letras[i];
+            }
+        }
+        return valor;
+    }
+    
+    public int cantidadLetras(){
+        return letras.length;
+    }
+
+}
---
+++ src/main/java/comunicacion/Fabula.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comunicacion;
+
+/**
+ *
+ * @author santi
+ */
+public class Fabula extends Escrito {
+
+    private String ensenanza;
+    private String interpretacion;
+
+    public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
+        super(origen, titulo, autor, paginas);
+        this.ensenanza = ensenanza;
+        this.interpretacion = interpretacion;
+    }
+
+    public String getEnsenanza() {
+        return ensenanza;
+    }
+
+    public void setEnsenanza(String ensenanza) {
+        this.ensenanza = ensenanza;
+    }
+
+    public String getInterpretacion() {
+        return interpretacion;
+    }
+
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
+
+    @Override
+    int palabrasTotales(int palabrasPagina) {
+        return super.getPaginas()*palabrasPagina;
+    }
+
+    @Override
+    String interpretacion() {
+        return interpretacion;
+    }
+
+    @Override
+    public String toString() {
+        return super.getOrigen() + "\n" + super.getTitulo() + "\n" + 
+            super.getAutor() + "\n" + super.getPaginas() + "\n" + ensenanza;
+    }
+}
